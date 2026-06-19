36a0295ad7c275d336fa35bb458084b2b506708c
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-06-19 17:45:15 +0300

    почистил всё

--- app/app.view.ts
+++ app/app.view.ts
@@ -1,27 +1,7 @@
 namespace $.$$ {
 
 	export class $bog_mediagram_app extends $.$bog_mediagram_app {
-		library_node() {
-			return this.$.$giper_baza_glob.home().land().Data( this.$.$bog_mediagram_library )
-		}
-		
-		entries_baza() {
-			const list = this.library_node().Entries()
-			if( !list ) return []
-			return list.remote_list() 
-		}
 
-		body_content() {
-			const tab = this.tab()
-			switch( tab ) {
-				case 'feed': return [ this.Feed_pane() ]
-				case 'circles': return [ this.Circles_pane() ]
-				case 'me': return [ this.Me_pane() ]
-				default: {
-					return [ this.Library_filters(), this.Library_banner(), this.Library_grid()]
-				}
-			}
-		}
 	}
 
 }
